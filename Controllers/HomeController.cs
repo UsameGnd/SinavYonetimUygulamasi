8e452e0ba26a6a5330732398b79550781d273ee6
Usame Gundogdu <Usame040404@hotmail.com>
2025-03-03 21:46:32 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SinavYonetimUygulamasi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
